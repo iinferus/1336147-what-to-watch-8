76db7f112f9d0b0edcb0c8181526fdde9449f3b2
Herman Krasnopolskyi <h.krasnopolskyi@phc.org.ua>
2021-10-19 09:29:14 +0300

    Добавляет страницы

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,19 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../main-screen/main-screen';
+
+type FilmCardProps = {
+  title: string;
+  genre: string;
+  releaseDate: number;
+}
+
+function App({title, genre, releaseDate}: FilmCardProps): JSX.Element {
+  return (
+    <MainScreen
+      title={title}
+      genre={genre}
+      releaseDate={releaseDate}
+    />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const FilmCardData = {
+  TITLE: 'The Grand Budapest Hotel',
+  GENRE: 'Drama',
+  RELEASE_DATE: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      title={FilmCardData.TITLE}
+      genre={FilmCardData.GENRE}
+      releaseDate={FilmCardData.RELEASE_DATE}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
